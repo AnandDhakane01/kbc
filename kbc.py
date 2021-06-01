e8df7184c28241ce4f77a9d32f2717f4730601c9
Anand Dhakane <ananddhakane01@gmail.com>
2021-06-01 16:04:06 +0530

    fixed bugs

--- kbc.py
+++ kbc.py
@@ -28,44 +28,60 @@ def kbc():
 
     reward = 0
     incorrect = False
-    question = 0
-    has_used_lifeLine = False
-    for i in range(len(QUESTIONS)):
+    index = 0
+    total = 15
+    has_used_lifeline = False
+
+    while index < 15:
+        if not has_used_lifeline:
+            print("You also have a 50-50 lifeline. Type 'lifeline' to use the lifeline.")
         print(f'Price Won: {reward}')
-        print(f'\tQuestion 1: {QUESTIONS[i]["name"]}')
+        print(f'\tQuestion 1: {QUESTIONS[index]["name"]}')
         print(f'\t\tOptions:')
-        print(f'\t\t\tOption 1: {QUESTIONS[i]["option1"]}')
-        print(f'\t\t\tOption 2: {QUESTIONS[i]["option2"]}')
-        print(f'\t\t\tOption 3: {QUESTIONS[i]["option3"]}')
-        print(f'\t\t\tOption 4: {QUESTIONS[i]["option4"]}')
+        print(f'\t\t\tOption 1: {QUESTIONS[index]["option1"]}')
+        print(f'\t\t\tOption 2: {QUESTIONS[index]["option2"]}')
+        print(f'\t\t\tOption 3: {QUESTIONS[index]["option3"]}')
+        print(f'\t\t\tOption 4: {QUESTIONS[index]["option4"]}')
         ans = input('Your choice ( 1-4 ) : ')
         # check for the input validations
         if ans == "quit":
             break
-        elif ans == "lifeline":
-            if not has_used_lifeLine and (i != 14):
-                has_used_lifeLine = True
-                question = lifeLine(QUESTIONS[i])
-                print(f'\tQuestion {i}: {question["name"]}')
+
+        if ans == "lifeline":
+            if not has_used_lifeline and (index != 14):
+                has_used_lifeline = True
+                question = lifeLine(QUESTIONS[index])
+                print(f'\tQuestion {index}: {question["name"]}')
                 print(f'\t\tOptions:')
                 print(f'\t\t\tOption 1: {question["option1"]}')
                 print(f'\t\t\tOption 2: {question["option2"]}')
                 print(f'\t\t\tOption 3: {question["option3"]}')
                 print(f'\t\t\tOption 4: {question["option4"]}')
-                ans = input('Your choice ( 1-4 ) : ')
+
+            elif has_used_lifeline:
+                print("You have already used your lifeline!!!!")
 
             else:
                 print("You can't use lifeline at this stage!!")
-                ans = input('Your choice ( 1-4 ) : ')
+            ans = input('Your choice ( 1-4 ) : ')
 
-        if isAnswerCorrect(QUESTIONS[i], int(ans)):
+        try:
+            int(ans)
+        except :
+            print("Invalid Input!!")
+            continue
+        if int(ans) not in range(1, 5):
+            print("Invalid Input!!")
+            continue
+
+        if isAnswerCorrect(QUESTIONS[index], int(ans)):
             # print the total money won.
-            reward = reward + QUESTIONS[i]["money"]
+            reward = reward + QUESTIONS[index]["money"]
             print(f'Total Money won: {reward}')
             # See if the user has crossed a level, print that if yes
-            if i == 5:
+            if index == 5:
                 print(f'you are now at level 1 and your min-reward is -/10000')
-            elif i == 10:
+            elif index == 10:
                 print(f'you are now at level 2 and your min-reward is -/320000')
             print('\nCorrect !')
 
@@ -74,21 +90,24 @@ def kbc():
             # also print the correct answer
             incorrect = True
             print('\nIncorrect !')
-            x = QUESTIONS[i]["answer"]
-            correct_ans = QUESTIONS[i][f'option{x}']
+            x = QUESTIONS[index]["answer"]
+            correct_ans = QUESTIONS[index][f'option{x}']
             print(f'The Correct answer is: {correct_ans}')
             break
+        index += 1
 
     # print the total money won in the end.
     if incorrect:
-        if i < 6:
+        if index < 6:
             print('You have won -/0')
         elif 6 <= i < 10:
             print("You have won -/10000")
-        elif i > 10:
+        elif index > 10:
             print("You have won -/320000")
     else:
         print(f'You have won -/{reward}')
+
+
 
 
 kbc()
